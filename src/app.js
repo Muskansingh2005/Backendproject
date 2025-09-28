0b4a3dcbc1910a201939350c6c7523aca4feae55
My Name <ms6464660@gmail.com>
2025-09-28 22:13:19 +0530

    add app code

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+export {app}
+//middleware - middleware is used to check the the reques are auhorized or not like the user whose send the request who is login or not 
